307262ab57557603449f90864c275c735102dccc
zakariaprofes <zakariaprofes@gmail.com>
2019-12-05 13:19:10 +0100

    Fixed change Skins

--- main.js
+++ main.js
@@ -6,163 +6,152 @@ let backButtons = document.querySelector(".backward");
 let previousOperandTextElement = document.querySelector(".previous-number");
 let currentOperandTextElement = document.querySelector(".current-number");
 class Calculator {
-	constructor(previousOperandTextElement, currentOperandTextElement) {
+	constructor(previousOperandTextElement,currentOperandTextElement) {
 		this.currentOperandTextElement = currentOperandTextElement;
 		this.previousOperandTextElement = previousOperandTextElement;
 		this.clear();
 		this.updateDisplay();
 	}
 
-	clear() {
-		this.currentOperand = "";
-		this.previousOperand = "";
-		this.operator = "";
+	clear(){
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operator = '';
 	}
 
-	backspace() {
-		this.currentOperand = this.currentOperand.slice(0, -1);
+	backspace(){
+		this.currentOperand = this.currentOperand.slice(0,-1);
 	}
 
-	totalLength(currentOperand) {
-		switch (this.currentOperand.length) {
+	totalLength(currentOperand){
+		switch(this.currentOperand.length){
 			case 6:
-				currentOperandTextElement.style.fontSize = "40px";
-				break;
+			currentOperandTextElement.style.fontSize = "40px";
+			break;
 			case 9:
-				currentOperandTextElement.style.fontSize = "28px";
-				break;
+			currentOperandTextElement.style.fontSize = "28px";
+			break;
 		}
 	}
 
-	concatNumber(number) {
-		if (this.currentOperand.length === 17) {
-			return;
-		}
-		if (number === "." && this.currentOperand.includes(".")) {
-			return;
-		}
+	concatNumber(number){
+		if(this.currentOperand.length === 17){return}
+		if(number === '.' && this.currentOperand.includes(".")){return}
 		this.currentOperand = this.currentOperand + number.toString();
 		this.totalLength(this.currentOperand);
 	}
 
-	operatorSelected(operator) {
-		this.operator = operator;
-		if (this.previousOperand !== "") {
-			this.compute();
-		}
-		if (this.currentOperand === "") {
-			return;
-		}
-		this.previousOperand = this.currentOperand;
-		console.log(this.previousOperand);
-		this.currentOperand = "";
+	operatorSelected(operator){
+ 		this.operator = operator;
+ 		if(this.previousOperand !== ""){
+ 			this.compute()
+ 		}
+ 		if(this.currentOperand === ""){return}
+ 		this.previousOperand = this.currentOperand;
+ 		console.log(this.previousOperand)
+ 		this.currentOperand = "";
 	}
 
-	compute() {
-		let computed = 0;
+	compute(){
+		var computed = 0 ;
 		var current = parseFloat(this.currentOperand);
 		var previous = parseFloat(this.previousOperand);
-		if (isNaN(current) || isNaN(previous)) {
-			return;
-		}
-		switch (this.operator) {
+		if(isNaN(current) || isNaN(previous)){return}
+		switch(this.operator){
 			case "+":
 				computed = current + previous;
-				break;
+			break;
 			case "-":
 				computed = previous - current;
-				break;
+			break;
 			case "*":
-				computed = current * previous;
-				break;
+				computed = previous * current;
+			break;
 			case "/":
 				computed = previous / current;
-				break;
+			break;	
 		}
 		this.currentOperand = Math.round(computed * 100) / 100;
 		this.previousOperand = "";
 		this.operator = "";
 	}
-
-	updateDisplay() {
+	
+	updateDisplay(){
 		this.currentOperandTextElement.innerText = this.currentOperand;
 		this.totalLength(this.currentOperand);
-		if (this.operator != null) {
-			this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operator}`;
+		if(this.operator != null){
+			this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operator}`;		
 		}
 	}
 }
 
-var calculator = new Calculator(
-	previousOperandTextElement,
-	currentOperandTextElement
-);
+var calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
 
 digitButtons.forEach(digit => {
-	digit.addEventListener("click", function() {
+	digit.addEventListener("click" , function() {
 		calculator.concatNumber(digit.innerText);
-		calculator.updateDisplay();
-	});
-});
+		calculator.updateDisplay()
+	})
+})       
 
 operatorButtons.forEach(button => {
 	button.addEventListener("click", function() {
 		calculator.operatorSelected(button.innerText);
-		calculator.updateDisplay();
-	});
-});
+		calculator.updateDisplay()
+	})
+})
 
 clearButtons.addEventListener("click", function() {
 	calculator.clear();
-	calculator.updateDisplay();
-});
+	calculator.updateDisplay()
+})
 
 backButtons.addEventListener("click", function() {
 	calculator.backspace();
-	calculator.updateDisplay();
-});
+	calculator.updateDisplay()
+})
 
 equalButtons.addEventListener("click", function() {
 	calculator.compute();
-	calculator.updateDisplay();
-});
+	calculator.updateDisplay()
+})
 
-document.addEventListener("keydown", function(event) {
-	switch (event.key) {
-		case "0":
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
-		case "7":
-		case "8":
-		case "9":
-		case ".":
-			calculator.concatNumber(event.key);
-			calculator.updateDisplay();
-			break;
-		case "+":
-		case "-":
-		case "*":
-		case "/":
-			calculator.operatorSelected(event.key);
-			calculator.updateDisplay();
-			break;
-		case "Enter":
-			calculator.compute();
-			calculator.updateDisplay();
-			break;
-		case "clear":
-			calculator.clear();
-			calculator.updateDisplay();
-			break;
-		case "Backspace":
-			calculator.backspace();
-			calculator.updateDisplay();
-			break;
-	}
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '.':
+            calculator.concatNumber(event.key);
+	    calculator.updateDisplay()
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        	calculator.operatorSelected(event.key);
+		calculator.updateDisplay()
+            break;
+        case 'Enter':
+           	calculator.compute();
+		calculator.updateDisplay()
+            break;
+        case 'clear':
+       		calculator.clear();
+		calculator.updateDisplay()
+        	break
+        case 'Backspace':
+           	calculator.backspace();
+		calculator.updateDisplay()
+            break;
+    }
 });
 
 function changeColor() {
@@ -172,5 +161,10 @@ function changeColor() {
 	link.type = "text/css";
 	link.href = "style2.css";
 	head.appendChild(link);
-	document.querySelector(".btn").value = "Wanna Go back ?";
+	document.querySelector(".btn").style.visibility = "hidden";
+	document.getElementById("Second").style.visibility = "visible";
+}
+
+function refresh() {
+	window.location.reload();
 }
